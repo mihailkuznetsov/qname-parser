87cc5c2c82753f037c04311cd326253aeacae26d
MihailKuznetsov <ztaticvienn@gmail.com>
2014-07-23 10:47:56 +0300

    Fixed ParserTest

--- src/test/java/com/codenvy/testtask/qname/ParserTest.java
+++ src/test/java/com/codenvy/testtask/qname/ParserTest.java
@@ -10,8 +10,9 @@ import static org.junit.Assert.fail;
  */
 public class ParserTest {
 
-    static final String[] validQNames = {"prefix:name","prefix:na me","name",};
-    static final String[] illegalQNames = {"",".","..","prefix:","prefix:name ","prefix: name", "pre fix:name", "name/name", "name[name", "prefix:name:name"};
+    static final String[] validQNames = {"prefix:name","prefix:na me","name","name[name"};
+    static final String[] illegalQNames = {"",".","..","prefix:","prefix:name ","prefix: name", "pre fix:name",
+            "name/name", "name[name", "prefix:name:name"};
 
     Parser parser = new Parser();
 
@@ -22,15 +23,17 @@ public class ParserTest {
         StringBuilder message = new StringBuilder();
 
         for (String validQName : validQNames) {
-            actualQName = null;
+            actualQName=null;
             try {
                 actualQName = parser.parse(validQName);
-            } catch (IllegalNameException e) {
-                message.append("\nIllegalNameException has been occured on parsing valid name \"" + validQName + "\"");
-            }
-            if (actualQName != null) {
                 assertEquals(validQName, actualQName.getAsString());
+            } catch (IllegalNameException e) {
+                message.append("\nIllegalNameException has been occured on parsing valid name \""
+		        + validQName + "\"");
             }
+        }
+        if (message.length()>0){
+            fail(message.toString());
         }
     }
 
@@ -42,9 +45,9 @@ public class ParserTest {
         for (String illegalQName : illegalQNames) {
             try {
                 parser.parse(illegalQName);
-                message.append("\nIllegalNameException is not thrown on parsing illegal name \"" + illegalQName + "\"");
+                message.append("\nIllegalNameException is not thrown on parsing illegal name \""
+		        + illegalQName + "\"");
             } catch (IllegalNameException e) {
-
             } catch (Exception e) {
                 message.append("\nUnexpected exception is thrown on parsing illegal name\"" + illegalQName + "\"");
             }
@@ -54,3 +57,4 @@ public class ParserTest {
         }
     }
 }
+
